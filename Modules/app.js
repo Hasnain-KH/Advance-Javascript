f9dfdbb19b913349514aa6d8ecdbdb64f5548e93
Muhammadhasnain <webdevhasnain@gmail.com>
2026-04-24 23:44:44 +0500

    Add some practice of module.

---
+++ Modules/app.js
@@ -0,0 +1,20 @@
+// import { Pi, added } from './math.js';
+
+// console.log(Pi);
+
+// console.log(added(1, 2, 3));
+
+
+// import { firstName } from "./math.js";
+// import { lastName } from "./math.js";
+
+// console.log(`${firstName}  ${lastName}`);
+
+import { car } from "./math.js";
+console.log(car.model.jjk?.ll);
+
+
+
+
+
+
---
+++ Modules/math.js
@@ -0,0 +1,15 @@
+export const Pi = 3000;
+
+export const firstName = "Hasnain";
+export const lastName = "Riaz";
+
+export function added(a,b,c){
+    return a + b + c;
+}
+
+
+export let car = {
+    name : "civic",
+    price : "2000",
+    model : 2025
+}
