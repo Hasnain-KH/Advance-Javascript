0eaa82d7d0cf52a4577ba539ebb95442867004a1
Muhammadhasnain <webdevhasnain@gmail.com>
2026-05-02 15:35:20 +0530

    Add Tasks of filter method

---
+++ Array-methods/app.js
@@ -0,0 +1,74 @@
+// let marks = [40, 55, 70, 30, 90];
+// let mymarks = marks.filter(function (num) {
+//         return num > 50;
+
+// })
+// console.log(mymarks);
+
+
+// let numbers = [10, 15, 20, 25, 30];
+// let odd = numbers.filter(function (numb) {
+//     return numb % 2 === 1;
+// })
+// console.log(odd);
+
+
+// let students = [
+//     { name: "Ali", marks: 45 },
+//     { name: "Ahmed", marks: 80 },
+//     { name: "Sara", marks: 60 },
+//     { name: "Hina", marks: 30 }
+// ];
+
+// let stu = students.filter(function (ma) {
+//     return ma >= 50
+// })
+// console.log(stu + "Passed in exam");
+
+
+// let numbers = [5, 12, 8, 20, 3, 18];
+
+// let num1 = numbers.filter(function (n) {
+//     if (n >= 10 && n % 2 === 0) {
+//         return n;
+//     }
+// })
+// console.log(num1);
+
+// let names = ["Ali", "Ahmed", "Usman", "Sara", "Hina"];
+// let lett = names.filter(function (letter) {
+//     if (letter === "A") {
+//         return letter;
+//     }
+// })
+// console.log(lett);
+
+
+// let products = [
+//     { name: "Phone", price: 500, inStock: true },
+//     { name: "Laptop", price: 1000, inStock: false },
+//     { name: "Mouse", price: 200, inStock: true }
+// ];
+// let object = products.filter(function (obj) {
+//     if (obj.inStock === true && obj.price > 300) {
+//         return obj;
+//     }
+// })
+// console.log(object);
+
+// let arr = [1, 2];
+
+// let names = ["Ali", "Ahmed", "Ayan", "Usman", "Sara", "Hina"];
+
+
+// let capital = names.filter((na) => {
+//     return na[0] === "A";
+// })
+
+// for (let i = 0; i < capital.length; i++) {
+//     let h2 = document.createElement("h2");
+//     h2.innerText = capital[i]
+//     document.body.append(h2);
+// }
+
+
