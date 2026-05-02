5682326789f0497a663d43b20a7b9717d0b4fe96
Muhammadhasnain <webdevhasnain@gmail.com>
2026-05-02 23:03:53 +0530

    Add some more tasks of filter method

--- Array-methods/app.js
+++ Array-methods/app.js
@@ -54,21 +54,73 @@
 //         return obj;
 //     }
 // })
-// console.log(object);
-
-// let arr = [1, 2];
+// for (let i = 0; i < object.length; i++) {
+//     for (let key in object[i]) {
+//         let h4 = document.createElement("h4");
+//         let h1 = document.createElement("h1");
+//         h4.innerText = key;
+//         h1.innerText = object[i][key];
+//         document.body.append(h1);
+//         document.body.append(h4);
+//     }
+// }
 
 // let names = ["Ali", "Ahmed", "Ayan", "Usman", "Sara", "Hina"];
-
-
 // let capital = names.filter((na) => {
 //     return na[0] === "A";
 // })
 
 // for (let i = 0; i < capital.length; i++) {
 //     let h2 = document.createElement("h2");
-//     h2.innerText = capital[i]
+//     h2.classList.add("h2");
+//     h2.innerText = capital[i];
 //     document.body.append(h2);
+
+//     h2.addEventListener("click", function () {
+//         if (document.body.style.backgroundColor === 'white') {
+//             document.body.style.backgroundColor = 'black';
+//         }
+//         else {
+//             document.body.style.backgroundColor = "white"
+//         }
+//     })
 // }
+
+
+// let stu = ["Ali", "Ahmed", "Ayan", "Usman", "Sara", "Hina"];
+
+// let mynames = names.filter((sa) => {
+//     return sa.startsWith("A");
+// })
+// console.log(mynames);
+
+// let names = ["Ali", "Ahmed", "Usman", "Sara", "Hina"];
+// let myname = names.filter((sav) => {
+//     return sav[sav.length - 1] === "a"
+// })
+
+// console.log(myname);
+
+
+// let orders = [
+//     { id: 1, amount: 500, status: "delivered" },
+//     { id: 2, amount: 200, status: "pending" },
+//     { id: 3, amount: 800, status: "delivered" },
+//     { id: 4, amount: 150, status: "cancelled" }
+// ];
+
+// let myorder = orders.filter((or) => {
+//     if (or.status === "delivered" && or.amount > 300) {
+//         console.log(or);
+//     }
+// })
+
+// let numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+// numbers.filter((num) => {
+//     if (num % 2 === 0 && num > 5) {
+//         console.log(num);
+//     }
+// })
+
 
 
