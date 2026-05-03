2faf85d13ba53f4ab943a9ddea46c62e89745fc8
Muhammadhasnain <webdevhasnain@gmail.com>
2026-05-03 16:53:12 +0500

    Add ternary operator examples with user input handling
    Practiced ternary operator in JavaScript with user input handling.
    Implemented conditions for different user roles (admin, user) and displayed results using DOM manipulation.

---
+++ Ternary-operator/app.js
@@ -0,0 +1,50 @@
+// let num = 0;
+// let re = num >= 0 ? "positve" : "Negative";
+// console.log(re);
+
+// let marks = 50;
+// let res1 = marks >= 50 ? "Passed" : "Failed";
+// console.log(res1);
+
+
+// let temp = 20;
+// let res3 = temp > 30 ? "Hot" : "Cool";
+// console.log(res3);
+
+// let num4 = Number(prompt("Enter a Number"));
+// let res4 = num4 % 2 === 0 ? "Even" : "Odd";
+// console.log(res4);
+
+// let inLoggedIn = prompt("Enter something");
+// let res5 = inLoggedIn === true ? "Welcome User" : "Please Login";
+// console.log(res5);
+
+
+// let input = prompt("Enter true or false");
+
+// let isLoggedIn = input === "true";
+
+// let res5 = isLoggedIn ? "wellcome" : "Please Login";
+
+// console.log(res5);
+
+// let per = prompt("enter yes or not").toLowerCase()
+// let res = per === "yes" ? "Access Granted" : "Access Denied";
+// console.log(res);
+
+
+// let inp = prompt("Enter your profession").toLowerCase()
+// let inpsave = inp;
+// let result = inp === "admin" ? "fullAccess" : inp === "user" ? "Limited Access" : "No Access";
+
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1");
+// let h2 = document.createElement("h2");
+// div.append(h2);
+// div.append(h1);
+// h2.innerText = inpsave + " :";
+// h1.innerText = result;
+// div.classList.add("div");
+// document.body.append(div);
+
+
