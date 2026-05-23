9f81aa037e49246d6919dd8f2db5dfc388b119eb
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-05-24 00:40:29 +0500

    Add one more Task of apply

--- call-apply-bind-pracice/app.js
+++ call-apply-bind-pracice/app.js
@@ -81,20 +81,61 @@
 
 // bind //
 
-const person = {
-    name: "Hasnain",
-    age: 20
+// const person = {
+//     name: "Hasnain",
+//     age: 20
+// };
+
+// const person2 = {
+//     name: "Ali",
+//     age: 25
+// };
+
+// const person3 = {
+//     name: "Ayan",
+//     age: 22
+// }
+
+// function introduce(city, country) {
+//     console.log(
+//         `My name is ${this.name}, I am ${this.age} years old. I live in ${city}, ${country}.`
+//     );
+// }
+
+// introduce.apply(person, ["Karachi", "Pakistan"]);
+// introduce.apply(person2, ["Lahore", "Pakistan"]);
+// introduce.apply(person3, ["Multan", "Pakistan"]);
+
+const app = document.querySelector("#app");
+
+const order1 = {
+    restaurant: "multan",
+    item: "Zinger Burger"
 };
 
-function introduce(city, country) {
+const order2 = {
+    restaurant: "Quidabad",
+    item: "Chicken Biryani"
+};
+
+
+function showOrder(customerName, city) {
     console.log(
-        `My name is ${this.name}, I am ${this.age} years old. I live in ${city}, ${country}.`
+        `The name of Restaurant is ${this.restaurant} and the Item is ${this.item} and The customer name is ${customerName} , and The city name of the Customer is ${city}`
     );
+
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+    The Restaurant name is <h1>${this.restaurant}</h1>
+    And the Item is <h1>${this.item}</h1>
+    And The Customer Name is <h1>${customerName}</h1>
+    And the customer city name is <h1>${city}</h1>
+    `
+    app.append(card);
 }
 
-let na = introduce.apply(person);
-na();
-
-
+showOrder.call(order1, "Hasnain", "Lahore");
+showOrder.apply(order2, ["Hasnain", "Lahore"]);
 
 
