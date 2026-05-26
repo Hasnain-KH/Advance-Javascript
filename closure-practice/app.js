5ea8bddf36e3cbd26e62c6ff63f8d8156ecfe7d3
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-05-26 15:02:07 +0500

    Added one more task of closure

--- closure-practice/app.js
+++ closure-practice/app.js
@@ -22,13 +22,48 @@
 // console.log(result());
 // console.log(result());
 
-function outer2() {
-    let name = "Hasnain";
-    function inner() {
-        console.log(name);
+// function outer2() {
+//     let name = "Hasnain";
+//     function inner() {
+//         console.log(name);
+//     }
+//     return inner;
+// }
+
+// let final = outer2();
+// final();
+
+
+// function abc() {
+
+//     let occupation = "Full Stack web developer"
+//     let firstName = "Hasnain";
+//     let LastName = "Riaz";
+//     function de() {
+//         console.log(`${occupation} ${firstName} ${LastName}`);
+//     }
+//     return de;
+// }
+
+// let print = abc();
+// print();
+
+
+
+function abc() {
+    let pro = prompt("Enter here your name ");
+    let occupatoin = "is a web developer"
+    function de() {
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        h1.innerText = `${pro} ${occupatoin}`
+        div.append(h1);
+        div.classList.add("div")
+        document.body.append(div)
     }
-    return inner;
+    return de;
 }
 
-let final = outer2();
-final();
+let print2 = abc();
+print2();
+
