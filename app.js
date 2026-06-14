0206d255b43aa253a459debce17cebe49058e187
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-06-14 22:19:32 +0500

    add some more practice of nullish operator

---
+++ app.js
@@ -0,0 +1,79 @@
+// null is a falsy value according to the nullish operator
+
+// undefined is a falsy value according to the nullish operator
+
+// let userScore = "hasnain";
+// let userNickname = "";
+// let userCity = null;
+
+// myuserScore = userScore ?? 100;
+// console.log(myuserScore);
+
+// userNickname = userNickname ?? "Anonymous";
+// console.log(userNickname);
+
+// userCity = userCity ?? "karachi";
+// console.log(userCity);
+
+
+// let settings = {
+//     theme: "Dark",
+//     fontSize: 0,
+//     autoSave: null
+// };
+
+
+// let s1 = settings.theme ?? "white";
+// let s2 = settings.fontSize ?? 16;
+// let s3 = settings.autoSave ?? "false";
+
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+
+// let originalPrice = 500;
+// let discountAmount = 0;
+// let deliveryCharges = null;
+
+// let discountAmou = discountAmount ?? 50;
+// console.log(`Thsi is the Discount Amount ${discountAmou}`);
+
+// let deliCharg = deliveryCharges ?? 150;
+// console.log(`Thsi is the deliveryCharges ${deliCharg}`);
+
+// let ori = originalPrice ?? 150;
+// console.log(`This is the origonal price ${ori}`);
+
+// let userName = "Kashif";
+// let userAge = 0;
+// let isPremium = false;
+// let lastLogin = undefined;
+
+// let user = userName ?? "Name is not defined";
+// console.log(user);
+
+
+
+// let userage = userAge ?? "18";
+// console.log(userage);
+
+// let premium = isPremium ?? "true";
+// console.log(premium);
+
+// let userlogin = lastLogin ?? "Never";
+// console.log(userlogin);
+
+// let val = "";
+
+// val = val ?? "Hasnain";
+// console.log(val);
+
+// let num = 0;
+// num = num ?? 10;
+// console.log(num);
+
+// let name = "sufyan";
+// name = name ?? "ayan";
+// console.log(name);
+
+
